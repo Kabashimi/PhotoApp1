15090267f39d25726bb3cb79499d9edce5f0e429
Johny Di <johny.dudek@gmail.com>
2015-01-07 09:01:13 +0100

    Added basic controls

--- src/com/example/photoapp/MainActivity.java
+++ src/com/example/photoapp/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -12,4 +13,18 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    
+    
+    private void TakePhotoBtnClick(View v){
+    	
+    }
+    
+    private void DrawBtnClick(View v){
+    	
+    }
+
+    private void SendPhotoBtnClick(View v){
+	
+}
+
 }
